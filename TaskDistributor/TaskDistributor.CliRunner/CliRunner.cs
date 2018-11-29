0b6b708b5db587a4446df0b54bf3b80f153e6ec7
Aliaksei <alexx333by@gmail.com>
2018-11-30 00:31:55 +0300

    CLI runner implemented

---
+++ TaskDistributor/TaskDistributor.CliRunner/CliRunner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TaskDistributor.CliRunner
+{
+    class CliRunner
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("usage: dotnet {0} <filename> <number of tasks>", AppDomain.CurrentDomain.FriendlyName);
+                return;
+            }
+
+            try
+            {
+                foreach (KeyValuePair<string, List<uint>> personTasks in TaskDistributor.Distribute(File.ReadAllLines(args[0]), uint.Parse(args[1])))
+                {
+                    Console.WriteLine("{0}: {1}", personTasks.Key, string.Join(", ", personTasks.Value.Select((task) => task.ToString())));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
