57f65b0df21673bdc587e146f7ed80927594ce15
Aliaksei <alexx333by@gmail.com>
2018-11-30 00:02:02 +0300

    Library implemented

---
+++ TaskDistributor/TaskDistributor/TaskDistributor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskDistributor
+{
+    public static class TaskDistributor
+    {
+        public static Dictionary<string, List<uint>> Distribute(List<string> people, uint tasksCount)
+        {
+            Dictionary<string, List<uint>> result = new Dictionary<string, List<uint>>(people.Count);
+
+            if (people.Count != 0)
+            {
+                uint minTasksCount = (uint)(tasksCount / people.Count), maxTasksCount = minTasksCount;
+                if (tasksCount % people.Count != 0)
+                {
+                    ++maxTasksCount;
+                }
+
+                foreach (string person in people)
+                {
+                    result[person] = new List<uint>();
+                }
+
+                List<string> copiedPeople = new List<string>(people);
+                Random random = new Random();
+                string chosenPerson;
+
+                for (uint i = 0; i < tasksCount; ++i)
+                {
+                    chosenPerson = copiedPeople[random.Next(copiedPeople.Count)];
+                    result[chosenPerson].Add(i + 1);
+
+                    if (result[chosenPerson].Count == maxTasksCount)
+                    {
+                        copiedPeople.Remove(chosenPerson);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
